4511ee85c57674726e1a368fd3e6d4ae1ed93474
salmantyagi <salmantyagi15@gmail.com>
2023-10-28 19:09:20 +0530

    react-node-build

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+import express from "express";
+import path from "path";
 
 const app = express();
 
-app.use(express.static('react_build'));
+app.use(express.static("react_build"));
 
-app.get('*', (req, res) => {
-  return res.sendFile(path.resolve(__dirname, './react_build/index.html'));
+app.get("*", (req, res) => {
+  return res.sendFile(path.resolve(__dirname, "./react_build/index.html"));
 });
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT);
+  console.log("Server started on port " + PORT);
 });
